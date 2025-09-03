612030684a2e93bc8cd95e9295b450c9d3d82d46
spanwite <spanwite@ya.ru>
2025-09-03 21:10:17 +0300

    Set meh and hyper keys in Fn and Shorts layers
    These keys are useful for AMD Software and PowerToys shortcuts

--- config/keymap/aliases/keys.h
+++ config/keymap/aliases/keys.h
@@ -3,19 +3,6 @@
 #define KP_SCREENSHOT  &kp PRINTSCREEN
 #define KP_SCREENSHOT_FULL  &kp LS(PRINTSCREEN)
 
-// Windows PowerToys
-#define PT_COL_PICK  MAKE_HYPER(F1)
-#define PT_RULER  MAKE_HYPER(F2)
-#define PT_EXTRACTOR  MAKE_HYPER(F3)
-#define PT_FANCY_ZONES  MAKE_HYPER(F4)
-#define PT_PIN  MAKE_HYPER(F5)
-#define PT_RUN  MAKE_HYPER(F6)
-
-// AMD Software
-#define AMD_RECORD  MAKE_MEH(Q)
-#define AMD_OVERLAY MAKE_MEH(W)
-#define AMD_SCRNSHT MAKE_MEH(E)
-
 // Utils
 #define ___ &trans
 #define XXX &none
