38a084cac878290eb6f28b58acf983d70ab906aa
spanwite <spanwite@ya.ru>
2025-09-06 17:23:04 +0300

    Rename keymap layers

--- config/keymap/aliases/layers.h
+++ config/keymap/aliases/layers.h
@@ -1,11 +1,11 @@
 #define ALPHA         0
-#define CAPS_WORD     1
+#define SHIFTED       1
 #define NUM_ROW       2
 #define NUMPAD        3
-#define SYMBOLS       4
+#define SYM           4
 #define NAV           5
 #define MOUSE         6
-#define POT_PLAYER    7
+#define MEH           7
 #define LOCK          8
 #define UNLOCK        9
 
@@ -17,8 +17,8 @@
 #define FN            14
 #define SHORTCUTS     15
 
-#define HOLD_LAYERS   NAV SHORTCUTS SYMBOLS FN
-#define TOGGLE_LAYERS ALPHA MOUSE NUMPAD NUM_ROW CAPS_WORD POT_PLAYER
+#define HOLD_LAYERS   NAV SHORTCUTS SYM FN
+#define TOGGLE_LAYERS ALPHA MOUSE NUMPAD NUM_ROW SHIFTED MEH
 #define GAME_LAYERS   GAME_DEFAULT GAME_DOTA2 GAME_EXTRA
 
 #define ALL_LAYERS    HOLD_LAYERS TOGGLE_LAYERS
